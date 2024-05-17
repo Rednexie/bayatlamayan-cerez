7d26004471854be3912d04dbda57fd8060388239
Rednexie <126991413+Rednexie@users.noreply.github.com>
2024-05-17 10:38:02 +0300

    new file:   cerez.sqlite 	modified:   db.js 	modified:   package-lock.json 	modified:   package.json

--- db.js
+++ db.js
@@ -5,6 +5,23 @@ const crypto = require('crypto');
 const STATIC_KEY = "13d60426-d8c7-46c4-a8b5-2cabe467";
 
 
+const LibSQL = require('libsql')
+const libsql = new LibSQL('cerez.sqlite')
+
+
+
+libsql.exec(`
+    CREATE TABLE IF NOT EXISTS leblebi(
+        id INTEGER PRIMARY KEY,
+        kullanici_adi TEXT NOT NULL UNIQUE,
+        parola TEXT,
+        cerez TEXT,
+        eposta TEXT,
+        rol INTEGER DEFAULT 0
+    )
+`)
+
+
 function encrypt(plaintext) {
   const cipher = crypto.createCipheriv('aes-256-ecb', Buffer.from(STATIC_KEY), null);
   let encrypted = cipher.update(plaintext, 'utf8', 'hex');
@@ -12,15 +29,37 @@ function encrypt(plaintext) {
   return encrypted;
 }
 
-const adam = faker.fakerTR.person;
-const username = adam.fullName().replace(' ', '_').toLowerCase()
-const email = faker.fakerTR.internet.email()
-const password = (faker.fakerTR.internet.password())
-const cookie = encrypt(btoa(require('crypto').randomUUID()))
-console.dir({
-    username,
-    email,
-    password,
-    cookie
-})
+
+for (let i = 0; i < 500; i++) {
+  const adam = faker.fakerTR.person;
+  if(adam.fullName().toLowerCase().includes('dr') || adam.fullName().toLowerCase().includes('prof') || adam.fullName().toLowerCase().includes('bay')){
+
+  }
+  else{
+    const username = faker.fakerTR.internet.userName().toLowerCase()
+    const email = faker.fakerTR.internet.email().toLowerCase()
+    const password = (faker.fakerTR.internet.password())
+    const cookie = encrypt(btoa(require('crypto').randomUUID()))
+    console.dir({
+        username,
+        email,
+        password,
+        cookie
+    })
+
+    libsql.prepare(`
+    INSERT INTO leblebi (
+      kullanici_adi,
+      parola,
+      cerez,
+      eposta,
+      rol
+    )
+    VALUES(
+      ?, ?, ?, ?, ?
+    )
+    `).run(username, password, cookie, email, 0)
+  }
+  
+}
 
